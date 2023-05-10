79ff17fc5b39205ddbe49e6500296667562bd9ad
carmencara <carmencgald@gmail.com>
2023-05-10 18:49:40 +0200

    .

---
+++ transmision.cpp
@@ -0,0 +1,44 @@
+//---------------------------------------------------------------------
+//                                                                    |
+//          ALGORITMO PARA RESOLVER LA ECUACIÓN DE SCHRÖDINGER        |
+//                   PROBLEMA VOLUNTARIO DEL TEMA 3                   |
+//                              Objetivos:                            |
+//                                                                    |
+//   1. Estimar el coeficiente de transmisión K simulando el sistema  |
+//      1E3 veces.                                                    |
+//   2. Comparar el valor de K y el de p_D(n_D) en t=n_D. Explicar    |
+//      la relación observada con argumentos físicos y por qué el     |
+//      método de maximización tiene sentido.                         |
+//   3. Estimar el error de K.                                        |
+//   4. Estudiar la dependencia de K en N (N=500,1000,2000)           |
+//   5. Estudiar la dependencia de K en V(x) (λ=0.1,0.3,0.5,1,5,10)   |
+//   6. Comparar con resultados teóricos.                             |
+//   7. Calcular los valores esperados de observables (posición,      |
+//      momento, energía cinética, energía total) con errores.        |
+//   8. Estudiar la dependencia de K con el número n de barreras y    |
+//      cuantificar este comportamiento haciendo un ajuste con una    |
+//      función de prueba.                                            |
+//                                                                    |
+//---------------------------------------------------------------------
+
+#include <iostream>
+#include <cmath>
+#include <fstream>     // Para trabajar con ficheros
+#include <complex>     // Para trabajar con números complejos
+
+#define N 1000         // Tamaño del retículo espacial
+#define PI 3.141593
+
+using namespace std;
+
+// FUNCIONES QUE SE VAN A UTILIZAR
+
+
+/*---------------------------------------------------------------------
+|                           FUNCIÓN PRINCIPAL                         |
+---------------------------------------------------------------------*/
+int main()
+{
+
+    return 0;
+}
